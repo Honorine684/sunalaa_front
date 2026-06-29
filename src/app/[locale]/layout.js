89cc926b3176a55eeaa8951925e8f53e699fbe89
Honorine <honorinegabiam0@gmail.com>
2026-06-29 17:56:34 +0100

    desktop menu change

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
-import { getMessages } from "next-intl/server";
+import { getMessages, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { AuthProvider } from "@/context/AuthContext";
@@ -17,10 +17,15 @@ const inter = Inter({
   preload: false,
 });
 
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
 export default async function LocaleLayout({ children, params }) {
   const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) notFound();
 
+  setRequestLocale(locale);
   const messages = await getMessages();
 
   return (
